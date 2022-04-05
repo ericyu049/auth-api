67316a39c9f819a84082e96d4c4790dd325eb6ed
Eric Yu <ericyu049@gmail.com>
2022-04-04 23:15:58 -0400

    jwt util

--- jwtUtil.js
+++ jwtUtil.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 
 const ALIVE_TIME = 15 * 60;
 const JWT_SECRETKEY = 'big penis';
@@ -15,4 +16,31 @@ function generateToken(user) {
         { expiresIn: ALIVE_TIME }
     );
     return token;
+}
+
+function getRefreshToken(user) {
+    const username = user.username;
+    const role = user.role;
+    const type = 'refresh';
+
+    const password = user.password;
+    const key = genKey(username, password);
+
+    const tokenPayload = { type, username, role, key };
+
+    const refreshToken = jwt.sign(tokenPayload, JWT_SECRET_KEY);
+    return refreshToken;
+}
+
+function hashHmacSha256(s) {
+  return crypto
+    .createHmac('sha256', JWT_SECRET_KEY)
+    .update(s)
+    .digest('hex');
+}
+
+function genKey(id, password) {
+    const rawKey = id + password;
+    const key = hashHmacSha256(rawKey, JWT_SECRET_KEY);
+    return key;
 }
